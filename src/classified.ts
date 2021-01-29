0cfb9750eb3bb8906f27081490ecfd220a680396
Lumm1t <lummit1337@gmail.com>
2021-01-29 17:54:51 +0100

    chore: add environment variables

---
+++ src/classified.ts
@@ -0,0 +1,41 @@
+import * as mysql from 'mysql';
+import * as Discord from 'discord.js';
+import { Sequelize } from 'sequelize';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const classified = {
+  getToken(): string {
+    return `${process.env.DISCORD_TOKEN}`;
+  },
+  getCon() {
+    const con = mysql.createConnection({
+      host: process.env.CONNECTION_HOST,
+      user: process.env.CONNECTION_USER,
+      password: process.env.CONNECTION_PASSWORD,
+      database: process.env.CONNECTION_DATABASE,
+    });
+
+    return con;
+  },
+  getSeq() {
+    const sequelize = new Sequelize(
+      `${process.env.CONNECTION_DATABASE}`,
+      `${process.env.CONNECTION_USER}`,
+      `${process.env.CONNECTION_PASSWORD}`,
+      {
+        host: `${process.env.CONNECTION_HOST}`,
+        dialect: 'mysql',
+        logging: false,
+      }
+    );
+
+    return sequelize;
+  },
+};
+
+const bot: Discord.Client = new Discord.Client();
+bot.login(classified.getToken());
+
+export default { classified, bot };
